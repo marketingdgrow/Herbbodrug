7743274d830ac4ac030156017ab3b9c00473ee11
Sivamani1812 <152249643+Sivamani1812@users.noreply.github.com>
2026-04-17 14:44:17 -0500

    update

--- src/components/HeroSectionHomepage/Hero.jsx
+++ src/components/HeroSectionHomepage/Hero.jsx
@@ -6,9 +6,9 @@ import {
   faBoxOpen, faTruck, faArrowRight, faChevronLeft, faChevronRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-const heroImg  = "/imgs/6.111.png";
-const slide2Img = "/imgs/5.111.png";
-const slide3Img = "/imgs/4.111.png";
+const heroImg  = "/imgs/11.111.png";
+const slide2Img = "/imgs/22.111.png";
+const slide3Img = "/imgs/33.111.png";
 
 const slides = [
   {
